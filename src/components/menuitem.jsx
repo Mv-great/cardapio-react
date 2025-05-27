5cc2e2bbeb0a39963816a480d8a726044e00cc86
Mv-great <viniliberato2008@gmail.com>
2025-05-27 08:19:38 -0300

    Add files via upload

--- src/components/menuitem.jsx
+++ src/components/menuitem.jsx
@@ -1,11 +1,24 @@
-// src/components/MenuItem.js
+// src/components/MenuItem.jsx
 import React from 'react';
 import './menuitem.css'; // We'll create this CSS file soon
 
 function MenuItem({ item }) {
+  // Construct a descriptive alt text. Using name and description if available.
+  const altText = `Foto de ${item.name}${item.description ? ': ' + item.description : ''}`;
+
   return (
     <div className="menu-item">
-      {item.image && <img src={item.image} alt={item.name} className="menu-item-image" />}
+      {/* Add descriptive alt, width, height, and keep lazy loading */}
+      {item.image && 
+        <img 
+          src={item.image} 
+          alt={altText} 
+          className="menu-item-image" 
+          width="100" 
+          height="100"
+          loading="lazy" // Keep lazy loading as it's good practice
+        />
+      }
       <div className="menu-item-details">
         <h3 className="menu-item-name">{item.name}</h3>
         <p className="menu-item-description">{item.description}</p>
@@ -16,3 +29,4 @@ function MenuItem({ item }) {
 }
 
 export default MenuItem;
+
